3c7d4b24156982e7fcb37a78c25adba88be7f31d
Brett Boston <brett.boston@stellar.org>
2025-07-07 16:52:41 -0700

    Subtraction test

--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -2,7 +2,7 @@ pub mod encryption;
 
 #[cfg(test)]
 mod tests {
-    use super::encryption::{decrypt_chunk, encrypt_chunk, add};
+    use super::encryption::{decrypt_chunk, encrypt_chunk, add, sub};
     use rand::rngs::StdRng;
     use rand::SeedableRng;
     use rust_elgamal as elgamal;
@@ -51,6 +51,34 @@ mod tests {
         let decrypted_amount = decrypt_chunk(&res_c, &privkey);
 
         assert_eq!(decrypted_amount, (x1 + x2) as u32);
+    }
+
+    // TODO: Write a test_binop function to dedup the add and sub tests
+    #[test]
+    fn test_sub() {
+        let mut rng = StdRng::from_entropy();
+        let privkey = elgamal::DecryptionKey::new(&mut rng);
+        let pubkey = privkey.encryption_key();
+
+        let x1= 42;
+        let r1 = elgamal::Scalar::random(&mut rng);
+
+        // Encrypt the amount
+        let c1= encrypt_chunk(&pubkey, x1, r1);
+
+        let x2 = 24;
+        let r2 = elgamal::Scalar::random(&mut rng);
+
+        // Encrypt the amount
+        let c2 = encrypt_chunk(&pubkey, x2, r2);
+
+        // Subtract the ciphertexts
+        let res_c = sub(&c1, &c2);
+
+        // Decrypt the ciphertext
+        let decrypted_amount = decrypt_chunk(&res_c, &privkey);
+
+        assert_eq!(decrypted_amount, (x1 - x2) as u32);
     }
 
 
