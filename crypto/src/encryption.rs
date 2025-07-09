8f94e90256dcf71d572ea6ddc4d7b23ddf17ef9f
Brett Boston <brett.boston@stellar.org>
2025-07-09 14:52:59 -0700

    Simple encrypt

--- crypto/src/encryption.rs
+++ crypto/src/encryption.rs
@@ -1,1 +1,10 @@
+use solana_zk_sdk::encryption::elgamal;
 
+
+pub fn encrypt_chunk(pubkey: &elgamal::ElGamalPubkey, amount: u16)
+    -> elgamal::ElGamalCiphertext
+{
+    // TODO: Should this function take a random value as input and use
+    // `encrypt_with` instead?
+    pubkey.encrypt(amount)
+}
